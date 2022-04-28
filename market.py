28e9e2c35957c25d0166ce5ab355ef8fdd7f711b
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-29 00:39:02 +0300

    build: added 3 dictionaries for table data

--- market.py
+++ market.py
@@ -9,4 +9,9 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    return render_template('market.html')
+    items = [
+        {'id':1, 'name': 'Phone', 'barcode':'95575949487839','price':500},
+        {'id':2, 'name': 'Laptop', 'barcode':'955759497365242','price':700},
+        {'id':3, 'name': 'Tablet', 'barcode':'9573902435936','price':900}
+    ]
+    return render_template('market.html', items=items)
