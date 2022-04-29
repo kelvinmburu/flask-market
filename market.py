ce556953392d2a0e5bad56cb814d35e3209fc181
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-30 01:57:27 +0300

    feat: replaced object with import all from db

--- market.py
+++ market.py
@@ -22,9 +22,5 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    items = [
-        {'id':1, 'name': 'Phone', 'barcode':'95575949487839','price':500},
-        {'id':2, 'name': 'Laptop', 'barcode':'955759497365242','price':700},
-        {'id':3, 'name': 'Tablet', 'barcode':'9573902435936','price':900}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
