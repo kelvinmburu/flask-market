f890cea26dfadf85be979f4e1296a825f536b8ee
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-30 01:39:18 +0300

    feat: imported sql_alchemy and added python class to hold objects

--- market.py
+++ market.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__) #Built-in variable that can be called for any variable file; can be called for any python file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+# Database python classes for classes
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    
+    def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route("/")
 @app.route("/home")
