72d0e690d5bbadbe49788bd15b1c33ef84aa3adf
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-30 13:49:26 +0300

    feat: added if function to check run.py run

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__) #Built-in variable that can be called for any variable file; can be called for any python file
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
+from market import app
+
+if __name__ == '__main__': # Checks if the run.py file has executed successfully
+    app.run(debug=True)
