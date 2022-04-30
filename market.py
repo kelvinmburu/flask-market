62bb54dd729380022592f58eb74be310243f2414
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-30 13:33:24 +0300

    refactor: transfered the code to routes file

--- market.py
+++ market.py
@@ -1,26 +1,2 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__) #Built-in variable that can be called for any variable file; can be called for any python file
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
 
-# Database python classes for classes
-class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024), nullable=False, unique=True)
-    
-    def __repr__(self):
-        return f'Item {self.name}'
 
-@app.route("/")
-@app.route("/home")
-def home_page():
-    return render_template('home.html')
-
-@app.route("/market")
-def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
